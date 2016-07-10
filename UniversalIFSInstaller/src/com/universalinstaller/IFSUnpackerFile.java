e0c60a119eabad70fe3654618868fa5e7003db45
Jordan Peacock <hewhocutsdown@gmail.com>
2016-07-09 21:55:09 -0500

    Create files.json if it does not exist.

--- UniversalIFSInstaller/src/com/universalinstaller/IFSUnpackerFile.java
+++ UniversalIFSInstaller/src/com/universalinstaller/IFSUnpackerFile.java
@@ -2,10 +2,14 @@ package com.universalinstaller;
 
 import java.io.File;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.List;
 
 import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
 
@@ -31,8 +35,14 @@ public class IFSUnpackerFile
 	public static List<IFSUnpackerFile> load() throws IFSUnpackerException
 	{
 		File configFile = new File("files.json");
-		if(!configFile.isFile())
-			throw new IFSUnpackerException(String.format("Config File %s Not Found", configFile.getAbsolutePath()));
+		if(!configFile.isFile()){
+			try{
+				IFSUnpackerFile sample = new IFSUnpackerFile(new File("sample.zip"),null,true,false,false);
+				String sampleJson = new GsonBuilder().setPrettyPrinting().create().toJson(Arrays.asList(sample));
+				Files.write(configFile.toPath(),sampleJson.getBytes(StandardCharsets.UTF_8));
+			} catch (IOException e){}
+			throw new IFSUnpackerException(String.format("Config File %s Not Found, A Sample Has Been Created.", configFile.getAbsolutePath()));
+		}
 
 		try{
 			String json = new String(Files.readAllBytes(configFile.toPath()),"UTF8");
